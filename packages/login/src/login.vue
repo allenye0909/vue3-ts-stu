d0d26f600fe4136fac1d5cfffc979556de72af05
allenye <allenye@aliyun.com>
2020-12-07 17:34:27 +0800

    调用 login组件

--- packages/login/src/login.vue
+++ packages/login/src/login.vue
@@ -3,18 +3,28 @@
  * @Author: allenye
  * @Email: allenye@aliyun.com
  * @Date: 2020-12-07 15:34:11
- * @LastEditTime: 2020-12-07 16:05:26
+ * @LastEditTime: 2020-12-07 16:55:03
 -->
 <template>
   <div>
     login 组件
+    <p>{{list}}</p>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, reactive, toRefs, ref } from "vue";
 export default defineComponent({
   name: "login",
+  setup() {
+    const data = reactive({
+      list: [1,2,3,4,5]
+    })
+
+    return {
+      ...toRefs(data)
+    }
+  }
 });
 </script>
 
